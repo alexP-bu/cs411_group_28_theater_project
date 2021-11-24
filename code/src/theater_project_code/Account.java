4947c0b991461dfb2cf0c9ae1a656145eb3e984b
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2021-11-24 13:51:33 -0500

    added methods to account_manager, added account class

--- code/src/theater_project_code/Account.java
+++
@@ -1,9 +0,0 @@
-package theater_project_code;
-
-public class Account {
-	private String username;
-	private String password;
-	private String type;
-	
-	
-}
---
+++ code/src/theater_project_code/account.java
@@ -0,0 +1,51 @@
+package theater_project_code;
+
+public class account {
+	public class java {
+
+	}
+	private String username;
+	private String password;
+	private String type;
+	private float balance;
+	
+	public String get_username() {
+		return username;
+	}
+	
+	public String get_password() {
+		return password;
+	}
+	
+	public void set_username(String username) {
+		this.username = username;
+	}
+	
+	public void set_password(String password) {
+		this.password = password;
+	}
+	
+	public void set_type(String type) {
+		this.type = type;
+	}
+	
+	public String get_type() {
+		return type;
+	}
+	
+	public float get_balance() {
+		return balance;
+	}
+	
+	public void set_balance(float balance) {
+		this.balance = balance;
+	}
+	
+	public void add_balance(float amount) {
+		this.balance += amount;
+	}
+	
+	public void subtract_balance(float amount) {
+		this.balance -= amount;
+	}
+}
