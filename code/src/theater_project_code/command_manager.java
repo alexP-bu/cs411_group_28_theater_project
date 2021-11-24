8763816c2b958ec14350075d2832c929e682fa04
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2021-11-24 15:57:02 -0500

    updated to use javas built in linkedlist

--- code/src/theater_project_code/command_manager.java
+++ code/src/theater_project_code/command_manager.java
@@ -1,7 +1,8 @@
 package theater_project_code;
+import java.util.LinkedList;
 
 public class command_manager {
-	private linked_list valid_commands = new linked_list();
+	private LinkedList<String> valid_commands = new LinkedList();
 	
 	//default constructor for the command manager has no valid commands
 	public command_manager() {
@@ -10,27 +11,27 @@ public class command_manager {
 	//constructor given input command and initial valid_commands
 	public command_manager(String input_command, String[] valid_commands_import) {
 		for (int i = 0; i < valid_commands_import.length - 1; i++) {
-			valid_commands.insert(valid_commands, valid_commands_import[i]);
+			valid_commands.add(valid_commands_import[i]);
 		}
 	}
 	//validates if a command is in the system, returns true if it is, false if it
 	public boolean validate_command(String keyword) {
-		if (valid_commands.contains(valid_commands, keyword)) {
+		if (valid_commands.contains(keyword)) {
 			return true;
 		}
 		return false;
 	}
 	//list valid commands for this class
 	public void list_valid_commands() {
-		valid_commands.print(valid_commands);
+		System.out.println(valid_commands.toString());
 	}
 	
 	public void delete_valid_command(String keyword) {
-		valid_commands.delete(valid_commands, keyword);
+		valid_commands.remove(keyword);
 	}
 	
 	public void add_valid_command(String keyword) {
-		valid_commands.insert(valid_commands, keyword);
+		valid_commands.add(keyword);
 	}
 	
 	public void run_command() {
