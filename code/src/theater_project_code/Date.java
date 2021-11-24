1707bc724a2d9c63f59eca3ae6e1b3dd64b08b36
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2021-11-24 16:38:43 -0500

    Create Date.java

---
+++ code/src/theater_project_code/Date.java
@@ -0,0 +1,47 @@
+package theater_project_code;
+
+public class Date {
+	private int month;
+	private int day;
+	private int year;
+	private int hours;
+	private int minutes;
+	private int seconds;
+	
+	public int get_month() {
+		return month;
+	}
+	public void set_month(int month) {
+		this.month = month;
+	}
+	public int get_day() {
+		return day;
+	}
+	public void set_day(int day) {
+		this.day = day;
+	}
+	public int get_year() {
+		return year;
+	}
+	public void set_year(int year) {
+		this.year = year;
+	}
+	public int get_hours() {
+		return hours;
+	}
+	public void set_hours(int hours) {
+		this.hours = hours;
+	}
+	public int get_minutes() {
+		return minutes;
+	}
+	public void set_minutes(int minutes) {
+		this.minutes = minutes;
+	}
+	public int get_seconds() {
+		return seconds;
+	}
+	public void set_seconds(int seconds) {
+		this.seconds = seconds;
+	}
+}
