a011f4299a004ba9a6748a60ca497124b7916a72
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2021-11-26 01:04:57 -0500

    fixed errors

--- code/src/theater_project_code/AccountManager.java
+++ code/src/theater_project_code/AccountManager.java
@@ -12,16 +12,15 @@ import java.io.EOFException;
 
 public class AccountManager {
 	
-	private HashMap<String, Account> accountList;
-	private Account loggedIn;
+	private HashMap<String, CustomerAccount> accountList;
+	private CustomerAccount loggedIn;
 	private File accountsFile;
 	
 	/*
-	 * initializing function for the account manager
+	 * initializing constructor for the account manager
 	 * get list of user names and passwords from database file and import them into list
 	 * 
 	 */
-	
 	public AccountManager() {
 		this.accountsFile = new File("accounts.ser");
 		if(!accountsFile.exists()) {
@@ -34,69 +33,56 @@ public class AccountManager {
 		this.loggedIn = null;
 		//read accounts from file and import them into accounts list
 		try {
-			this.accountList = new HashMap<String,Account>(importAccounts(accountsFile));
+			this.accountList = new HashMap<String,CustomerAccount>(importAccounts(accountsFile));
 		} catch (Exception e) {
 			System.out.println("error importing accounts file");
 			e.printStackTrace();
 		}
-	}
-	
+	}	
 	/*
 	 * login account using user name and password.
 	 * return true on successful login, throws exception if an error occurred.
-	 */
-	
+	 */	
 	public boolean loginAccount(String username, String password) throws IllegalArgumentException {
 		if(accountList.containsKey(username)) {
 			if(accountList.get(username).get_password().equals(password)) {
 				if(loggedIn == null) {
-					//if no other account logged in, login account. else, logout and login
+					//if no other account logged in, login account
 					loggedIn = accountList.get(username);
+					System.out.println("Account successfully logged in!");
 					return true;
 				} else {
-					logoutAccount();
-					loggedIn = accountList.get(username);
-					return true;
+					//another account is logged in already
+					System.out.println("Please log out before logging in a new account!");
+					return false;
 				}
-			}else {
-				System.out.println("Invalid password entered!");
 			}
-		}else {
-			System.out.println("Invalid username entered!");
 		}
 		return false;
 	}
-	
 	/*
 	 * logout account using user name. returns true on successful logout, false otherwise 
-	 */
-	
+	 */	
 	public boolean logoutAccount() {
 		loggedIn = null;
 		return true;
 	}
-	
 	/*
 	 * check if account is logged in. returns true if it is, false if not
 	 */
-	
-	public boolean isLoggedIn(String username) {
+	public boolean isLoggedIn() {
 		if(loggedIn == null) {
 			return false;
-		}
-		if(loggedIn.get_username().equals(username)) {
+		}else {
 			return true;
 		}
-		return false;
-	}
-	
+	}	
 	/*
 	 * method to add new account
 	 * returns true on successful account creation
 	 * throws exception on account creation failure
 	 * 
 	 */
-	
 	public boolean createCustomerAccount(String username, String password) {
 		if(accountList.containsKey(username)) {
 			System.out.println("Username already exists, please enter a different one!");
@@ -108,32 +94,52 @@ public class AccountManager {
 		//clear current account database file and export new one
 		clearDatabase();
 		exportAccounts(accountsFile, accountList);
-		//export local account list to update database file
-		exportAccounts(accountsFile, accountList);
 		//login account
 		this.loginAccount(username, password);
 		return true;
 	}
-	
+	/*
+	 * method to create administrator account
+	 */
+	public boolean createAdminAccount(String username, String password) {
+		if(createCustomerAccount(username, password)) {
+			accountList.get(username).set_type("administrator");
+			clearDatabase();
+			exportAccounts(accountsFile, accountList);
+			//login
+			logoutAccount();
+			this.loginAccount(username, password);
+			return true;
+		}
+		return false;
+	}
+	/*
+	 * method to return logged in account
+	 */
+	public CustomerAccount getAccount() {
+		if (loggedIn != null) {
+			return loggedIn;
+		}else {
+			return null;
+		}
+	}
 	/*
 	 * method to delete account by user name
 	 * account must be logged in to delete itself
 	 * administrator account must be logged in order to delete it
 	 * returns true on successful deletion, false otherwise
 	 */
-	
 	public boolean deleteAccount(String username) throws IllegalArgumentException {
 		if(loggedIn != null) {
 			if(loggedIn.get_type().equals("administrator") || loggedIn.get_username().equals(username)) {
+				@SuppressWarnings("resource")
 				Scanner reader = new Scanner(System.in);
 				System.out.printf("Are you sure you want to delete %s's account?\n", username);
 				System.out.println("Please re-enter account username to confirm: ");
 				if (!reader.nextLine().equals(username)) {
 					System.out.println("Account failed to delete!");
-					reader.close();
 					return false;
 				}
-				reader.close();
 				//delete account from accountList
 				accountList.remove(username);
 				//export new database file with deleted account
@@ -144,26 +150,23 @@ public class AccountManager {
 		}
 		return false;
 	}
-	
 	public void printAccountList() {
 		System.out.println("Accounts List: ");
-		for(Map.Entry<String, Account> entry : accountList.entrySet()) {
+		for(Map.Entry<String, CustomerAccount> entry : accountList.entrySet()) {
 			System.out.println(entry.getKey());
 		}
 		System.out.println("Accounts list done printing.");
 	}
-	
 	/*
 	 * method to read accounts from accounts list using FileInputStreams
-	 */
-	
-	private static HashMap<String,Account> importAccounts(File file) throws IOException, ClassNotFoundException {
-		HashMap<String,Account> output = new HashMap<String,Account>();
+	 */	
+	private static HashMap<String,CustomerAccount> importAccounts(File file) throws IOException, ClassNotFoundException {
+		HashMap<String,CustomerAccount> output = new HashMap<String,CustomerAccount>();
 		try {
 			ObjectInputStream reader = new ObjectInputStream(new FileInputStream(file));
 			while(true) {
 				try {
-					Account get = (Account) reader.readObject();
+					CustomerAccount get = (CustomerAccount) reader.readObject();
 					output.put(get.get_username(),get);
 				}catch (EOFException e) {
 					break;
@@ -176,15 +179,13 @@ public class AccountManager {
 		}
 		return output;
 	}
-	
 	/*
 	 * method to export accounts from account list in current session to file
 	 */
-	
-	private static void exportAccounts(File file, HashMap<String,Account> accountList) {
+	private static void exportAccounts(File file, HashMap<String,CustomerAccount> accountList) {
 		try {
 			ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(file));
-			for(Map.Entry<String, Account> entry : accountList.entrySet()) {
+			for(Map.Entry<String, CustomerAccount> entry : accountList.entrySet()) {
 				Account account = entry.getValue();
 				writer.writeObject(account);
 				writer.flush();
@@ -195,12 +196,10 @@ public class AccountManager {
 			e.printStackTrace();
 		}
 	}
-	
 	/*
 	 * method to database clear file
 	 */
-	
-	private void clearDatabase() {
+	public void clearDatabase() {
 		try {
 			new FileOutputStream(accountsFile).close();
 		}catch (Exception e) {
@@ -209,19 +208,18 @@ public class AccountManager {
 		}
 		System.out.println("Database file cleared.");
 	}
-	
 	/*
 	 * method to clear local account list
 	 */
-	
 	public void clearAccountList() {
 		accountList.clear();
 	}
 	
+	
 	/*
 	 * test harness code
-	 */
-	/*
+	 *
+	
 	public static void main(String[] args) {
 		//make a new account manager
 		AccountManager accountManager = new AccountManager();
