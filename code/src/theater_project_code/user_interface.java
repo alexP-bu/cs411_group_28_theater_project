2f1585f022f45d0ba8b4d2d622e468b2a388847a
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2021-11-26 01:04:45 -0500

    updated interface

--- code/src/theater_project_code/user_interface.java
+++ code/src/theater_project_code/user_interface.java
@@ -5,26 +5,49 @@ import java.util.Scanner;
 public class user_interface {
 	
 	public static void main(String[] args) {
+		/*
+		 * initialize system objects
+		 */
+		
+		String[] commandList = {"login", "logout", "new", "delete", "listAccounts", "newAdmin", "clearData"};
+		CommandManager commandManager = new CommandManager(commandList);
+		Scanner readInput = new Scanner(System.in);
+		String command = "";
+		printWelcomeMessage();
+		
+		/*
+		 * program loop
+		 */
+		
+		do {
+		printInstructions();
+		command = readInput.nextLine();
+		System.out.printf("The command you entered is: %s \n", command);
+		//check for valid user input command
+		if(commandManager.validateCommand(command)) {
+			commandManager.runCommand(command);
+		}else {
+			System.out.println("Invalid command entered. Please try again.");
+		}
+		}while(!command.equals("exit"));
+		
+		System.out.println("Goodbye.");
+		readInput.close();
+	}
+	
+	public static void printWelcomeMessage() {
 		System.out.println("*********************************");
 		System.out.println("Hello, and welome to the theater!");
 		System.out.println("*********************************");
-		
+		System.out.println();
+	}
+	
+	public static void printInstructions() {
+		System.out.println();
 		System.out.println("To login to your account, please type \"login\"");
+		System.out.println("To logout of your account, please type \"logout\"");
 		System.out.println("To view showtimes, please type \"showtimes\"");
 		System.out.println("To create a new account, please type \"new\"");
-		System.out.println("To shut down the system, please type \"exit\"");
-		
-		String command = "";
-		Scanner readInput = new Scanner(System.in);
-		
-		do {
-		command = readInput.nextLine();
-		System.out.printf("The command you entered is: %s\n", command);
-		//check for valid user input
-		
-		
-		}while(!command.equals("exit"));
-		
-		readInput.close();
+		System.out.println("To shut down the system, please type \"exit\"\n");
 	}
 }
