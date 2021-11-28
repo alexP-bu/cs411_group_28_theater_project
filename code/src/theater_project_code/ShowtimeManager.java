a9cc241ad72e1b5cf3c93121221880e19c7f25a0
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2021-11-28 13:09:46 -0500

    Update ShowtimeManager.java

--- code/src/theater_project_code/ShowtimeManager.java
+++ code/src/theater_project_code/ShowtimeManager.java
@@ -1,5 +1,6 @@
 package theater_project_code;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ShowtimeManager {
 	private ArrayList<Showtime> showtimes;
@@ -10,7 +11,9 @@ public class ShowtimeManager {
 	public ShowtimeManager() {
 		showtimes = new ArrayList<Showtime>();
 	}
-	
+	/*
+	 * list show times
+	 */
 	public void listShowtimes() {
 		if(showtimes != null) {
 			if(!showtimes.isEmpty()) {
@@ -20,5 +23,30 @@ public class ShowtimeManager {
 				}
 			}
 		}
+	}
+	/*
+	 * add show time
+	 * returns true on successful add, false otherwise
+	 */
+	public boolean addShowtime(Showtime show) {
+		if(show != null) {
+			showtimes.add(show);
+		}
+		return false;
+	}
+	/*
+	 * test harness code
+	 */
+	public static void main(String[] args) {
+		ShowtimeManager showtimes = new ShowtimeManager();
+		Showtime show1 = new Showtime(new Date(), "Endgame", new ArrayList<Theater>(List.of(new Theater(1), new Theater(2), new Theater(3))));
+		Showtime show2 = new Showtime(new Date(), "Infinity War", new ArrayList<Theater>(List.of(new Theater(1), new Theater(2), new Theater(3))));
+		Showtime show3 = new Showtime(new Date(), "Breaking Bad", new ArrayList<Theater>(List.of(new Theater(1), new Theater(2), new Theater(3))));
+		Showtime show4 = new Showtime(new Date(), "The Simpsons", new ArrayList<Theater>(List.of(new Theater(1), new Theater(2), new Theater(3))));
+		showtimes.addShowtime(show1);
+		showtimes.addShowtime(show2);
+		showtimes.addShowtime(show3);
+		showtimes.addShowtime(show4);
+		showtimes.listShowtimes();
 	}
 }
