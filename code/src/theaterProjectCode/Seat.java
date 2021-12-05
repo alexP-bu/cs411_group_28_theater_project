d448cf454ca115fc51777083accebe99f29c97f8
jjkuang <58530134+TiredSlowly@users.noreply.github.com>
2021-12-05 15:31:51 -0500

    Changes

--- code/src/theaterProjectCode/Seat.java
+++ code/src/theaterProjectCode/Seat.java
@@ -10,92 +10,95 @@ public class Seat implements Serializable {
 	private char row;
 	private int column;
 	private Account reservedBy;
-	
+
 	/*
 	 * constructors
 	 */
-	
+
 	public Seat(Showtime show, char row, int column, Account account) {
 		this.row = row;
 		this.column = column;
 		this.reservedBy = account;
 	}
-	
+
 	public Seat(char row, int column) {
 		this.row = row;
 		this.column = column;
 		this.reservedBy = null;
 	}
-	
+
 	public Seat() {
 		this.row = 0;
 		this.column = 0;
 		this.reservedBy = null;
 	}
+
 	/*
 	 * getters
 	 */
 	public char getRow() {
 		return this.row;
 	}
-	
+
 	public int getColumn() {
 		return this.column;
 	}
-	
+
 	public Account getReservedBy() {
 		return this.reservedBy;
 	}
-	
+
 	public boolean is_reserved() {
-		if(this.getReservedBy() != null) {
+		if (this.getReservedBy() != null) {
 			return true;
 		}
 		return false;
 	}
+
 	/*
 	 * setters/mutators
 	 */
 	public void setRow(char row) {
 		this.row = row;
 	}
-	
+
 	public void setColumn(int column) {
 		this.column = column;
 	}
+
 	/*
 	 * function to reserve seat given with account
 	 */
 	public boolean reserveSeat(Account account) {
-		if(this.reservedBy != null) {
+		if (this.reservedBy == null) {
 			this.reservedBy = account;
-			//System.out.printf("Seat successfully reserved by %s", account.getUsername());
+			// System.out.printf("Seat successfully reserved by %s", account.getUsername());
 			return true;
-		}else {
+		} else {
 			System.out.println("Seat already reserved. Please select a different seat!");
 			return false;
 		}
 	}
+
 	/*
 	 * function to reserve seat without account - use guest account
 	 */
 	public boolean reserveSeat() {
-		if(this.reservedBy == null) {
+		if (this.reservedBy == null) {
 			this.reservedBy = new Account();
 			System.out.println("Seat successfully reserved.");
 			return true;
-		}else {
+		} else {
 			System.out.println("Seat already reserved. Please select a different seat!");
 			return false;
 		}
 	}
-	
 
 	@Override
 	public String toString() {
-		if(this.reservedBy != null) {
+		if (this.reservedBy != null) {
 			return "[X]";
-		}else {
+		} else {
 			return "[O]";
 		}
 	}
--- code/src/theaterProjectCode/Ticket.java
+++ code/src/theaterProjectCode/Ticket.java
@@ -2,7 +2,7 @@ package theaterProjectCode;
 
 import java.io.Serializable;
 
-public class Ticket implements Serializable{
+public class Ticket implements Serializable {
 	/**
 	 * id
 	 */
@@ -12,7 +12,7 @@ public class Ticket implements Serializable{
 	private Seat seat;
 	private double price;
 	private Date datePurchased = null;
-	
+
 	public Ticket(Showtime show, Seat seat, double price, Date datePurchased, String theaterID) {
 		this.show = show;
 		this.seat = seat;
@@ -20,39 +20,46 @@ public class Ticket implements Serializable{
 		this.datePurchased = datePurchased;
 		this.theater = theaterID;
 	}
-	
+
 	public Ticket() {
 		this.show = null;
 		this.seat = null;
 		price = 0.00;
 		this.datePurchased = null;
 	}
-	
+
 	/*
 	 * getters/setters
 	 */
-	
+
 	public Showtime get_show() {
 		return show;
 	}
+
 	public void set_show(Showtime show) {
 		this.show = show;
 	}
+
 	public Seat get_seat() {
 		return seat;
 	}
+
 	public void set_seat(Seat seat) {
 		this.seat = seat;
 	}
+
 	public double get_price() {
 		return price;
 	}
+
 	public void set_price(double price) {
 		this.price = price;
 	}
+
 	public Date get_datePurchased() {
 		return datePurchased;
 	}
+
 	public void set_datePurchased(Date datePurchased) {
 		this.datePurchased = datePurchased;
 	}
@@ -64,6 +71,10 @@ public class Ticket implements Serializable{
 	public void setTheater(String theater) {
 		this.theater = theater;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "\n" + this.show.getName() + " Showing on " + this.show.getDate() + "\nPrice: " + this.show.getPrice();
+	}
+
 }
